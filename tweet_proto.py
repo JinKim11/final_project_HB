90775d090a7dff406e0f486ffc25f3597f4121fb
Jennifer Kim <jennkim1186@gmail.com>
2016-09-20 02:22:51 -0700

    first

---
+++ tweet_proto.py
@@ -0,0 +1,22 @@
+# Psuedocode
+
+tweet_list = []
+
+def read_text_file():
+	pass
+
+def split_file_and_add_to_list():
+	pass
+
+def write_to_file():
+	pass
+
+def post_tweets():
+	pass
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+	main()
